f654c45166c90211cb3bf6051caaac6eb12a41a6
wa1tnr <wa1tnr@users.noreply.github.com>
2022-03-22 10:29:26 +0000

    exact turnkey version
      fairly useful memory dumper
    
      rudamentary - no organization
      no way to tell what the non-printables are
    
    	modified:   dump.fs
    
    On branch dvlp-aa

--- mecrisp-stellaris/microbit/dump.fs
+++ mecrisp-stellaris/microbit/dump.fs
@@ -1,16 +1,15 @@
 \ dump.fs
-\ Tue 22 Mar 01:33:37 UTC 2022
+\ Tue 22 Mar 10:15:02 UTC 2022
 
 hex
-: noprint $2e emit ;
+: np. $2e emit ;
 
-: getchbb ( addr -- addr+1 )
-  dup \ hang onto address
-  c@
-  dup  $20 < if noprint drop 1 + exit then
-  dup  $7e > if noprint drop 1 + exit then
-  emit 1 + ;
-
+\ : getchbb ( addr -- addr+1 )
+\   dup \ hang onto address
+\   c@
+\   dup  $20 < if noprint drop 1 + exit then
+\   dup  $7e > if noprint drop 1 + exit then
+\   emit 1 + ;
 
 : isram? ( addr -- bool )
   dup
@@ -33,7 +32,7 @@ hex
   0
   1 -
   > if
-      $E000
+      $40000
       <
       \ ." was rom "
       exit
@@ -42,27 +41,29 @@ hex
   0
   ;
 
-: goget ( addr -- addr+1 )
+: m. ( addr -- addr+1 )
   dup
   c@
-  dup  $20 < if noprint drop 1 + exit then
-  dup  $7e > if noprint drop 1 + exit then
+  dup  $20 < if np. drop 1 + exit then
+  dup  $7e > if np. drop 1 + exit then
   emit 1 +
 ;
 
-: getch ( addr -- addr+1 )
-  dup isram? if goget exit then
-  dup isrom? if goget exit then
+\ : runz 0 do gm. loop ; ( n -- )
+\ : r 200 runz cr .s cr ;
+\ decimal
+\ -99 -98 -97
+\ hex
+\ 1 1 1
+\ 20000000
+\ 40000
+\ .s
+
+: gm. ( addr -- addr+1 )
+  dup isram? if m. exit then
+  dup isrom? if m. exit then
   $3f
   emit \ ERROR '?'
   exit
 ;
-
-: runz ( n -- )
-  0 do getch loop ;
-
-99 98 97 1 1 1
-hex
-20000000
-.s
 
