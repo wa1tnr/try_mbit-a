1baeaf18778122f4ee93fdf1b07178d62a70e911
wa1tnr <wa1tnr@users.noreply.github.com>
2022-03-22 01:41:00 +0000

    protections for over/under addr
      end of rom is assumed $e000
      end of ram is assumed $20000000 + $4000
    
    	modified:   dump.fs
    
    On branch dvlp-aa

--- mecrisp-stellaris/microbit/dump.fs
+++ mecrisp-stellaris/microbit/dump.fs
@@ -1,7 +1,10 @@
+\ dump.fs
+\ Tue 22 Mar 01:33:37 UTC 2022
+
 hex
 : noprint $2e emit ;
 
-: getch ( addr -- addr+1 )
+: getchbb ( addr -- addr+1 )
   dup \ hang onto address
   c@
   dup  $20 < if noprint drop 1 + exit then
@@ -15,25 +18,48 @@ hex
   1 -
   >
   if
-      $20004000 1 +
+      $20004000
       <
+      \ ." was ram "
       exit
   then
   drop
   0
   ;
 
-: getch ( addr -- addr+1 )
-  dup \ hang onto address
+: isrom? ( addr - bool )
+  \ ." entry isrom? condx: " .s ."  " cr
+  dup
+  0
+  1 -
+  > if
+      $E000
+      <
+      \ ." was rom "
+      exit
+  then
+  drop
+  0
+  ;
+
+: goget ( addr -- addr+1 )
+  dup
   c@
   dup  $20 < if noprint drop 1 + exit then
   dup  $7e > if noprint drop 1 + exit then
-  emit 1 + ;
+  emit 1 +
+;
 
+: getch ( addr -- addr+1 )
+  dup isram? if goget exit then
+  dup isrom? if goget exit then
+  $3f
+  emit \ ERROR '?'
+  exit
+;
 
 : runz ( n -- )
   0 do getch loop ;
-
 
 99 98 97 1 1 1
 hex
